f1f4b56a547c9298ba5627ca828f9429e69ae071
MdIftikharIslam <128501847+MdIftikharIslam@users.noreply.github.com>
2023-04-25 16:11:23 +0600

    Update settings.py

--- emailsender/settings.py
+++ emailsender/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ifticdr122@gmail.com'
-EMAIL_HOST_PASSWORD = 'blxezkbzbyonuhjr'
+EMAIL_HOST_USER = 'your_email_address_goes_here'
+EMAIL_HOST_PASSWORD = 'your_password_goes_here'
